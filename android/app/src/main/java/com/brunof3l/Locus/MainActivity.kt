346e423e3a99452daac9f743f7274ce6e39adfbd
Locus Upload <upload@example.com>
2025-11-04 07:49:09 -0400

    backup antes de arrumar deps e android

--- android/app/src/main/java/com/brunof3l/Locus/MainActivity.kt
+++ android/app/src/main/java/com/brunof3l/Locus/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.brunof3l.locus
 
+import android.os.Build
 import android.os.Bundle
+
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
-// Adicione esta importação
 import expo.modules.ReactActivityDelegateWrapper
 
 class MainActivity : ReactActivity() {
-
   override fun onCreate(savedInstanceState: Bundle?) {
-    // Adicionado para o react-native-screens
+    // Set the theme to AppTheme BEFORE onCreate to support
+    // coloring the background, status bar, and navigation bar.
+    // This is required for expo-splash-screen.
+    setTheme(R.style.AppTheme);
     super.onCreate(null)
   }
 
@@ -20,24 +23,39 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "Locus" // Certifique-se que "Locus" é o nome no app.json
+  override fun getMainComponentName(): String = "main"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture (Fabric) with a single boolean flag.
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate {
     return ReactActivityDelegateWrapper(
-      this,
-      BuildConfig.DEBUG,
-      object : DefaultReactActivityDelegate(
-        this,
-        mainComponentName,
-        // Habilita a Nova Arquitetura (Fabric) se estiver usando
-        DefaultNewArchitectureEntryPoint.fabricEnabled,
-        // Habilita o Modo Bridge se a Nova Arquitetura estiver desabilitada
-        DefaultNewArchitectureEntryPoint.jsBundleEntryFile
-      ) {}
-    )
+          this,
+          BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
+          object : DefaultReactActivityDelegate(
+              this,
+              mainComponentName,
+              fabricEnabled
+          ){})
+  }
+
+  /**
+    * Align the back button behavior with Android S
+    * where moving root activities to background instead of finishing activities.
+    * @see <a href="https://developer.android.com/reference/android/app/Activity#onBackPressed()">onBackPressed</a>
+    */
+  override fun invokeDefaultOnBackPressed() {
+      if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R) {
+          if (!moveTaskToBack(false)) {
+              // For non-root activities, use the default implementation to finish them.
+              super.invokeDefaultOnBackPressed()
+          }
+          return
+      }
+
+      // Use the default back button implementation on Android S
+      // because it's doing more than [Activity.moveTaskToBack] in fact.
+      super.invokeDefaultOnBackPressed()
   }
 }
--- android/app/src/main/java/com/brunof3l/Locus/MainApplication.kt
+++ android/app/src/main/java/com/brunof3l/Locus/MainApplication.kt
@@ -1,58 +1,55 @@
 package com.brunof3l.locus
 
 import android.app.Application
+import android.content.res.Configuration
+
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
-import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
-import com.facebook.react.defaults.DefaultReactHost
+import com.facebook.react.ReactHost
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.flipper.ReactNativeFlipper
-import com.facebook.soloader.SoLoader // <-- ESTA LINHA ESTAVA COM O ERRO "comimport"
+import com.facebook.soloader.SoLoader
 
-// Adicione estas importações do Expo
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
 
 class MainApplication : Application(), ReactApplication {
 
   override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
-    this,
-    object : DefaultReactNativeHost(this) {
-      override fun getPackages(): List<ReactPackage> {
-        // Packages auto-linked by RN CLI
-        val packages = PackageList(this).packages
-        // Adicione pacotes manuais aqui, se necessário
-        return packages
+        this,
+        object : DefaultReactNativeHost(this) {
+          override fun getPackages(): List<ReactPackage> {
+            // Packages that cannot be autolinked yet can be added manually here, for example:
+            // packages.add(new MyReactNativePackage());
+            return PackageList(this).packages
+          }
+
+          override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+
+          override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+          override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+          override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
-
-      override fun getJSMainModuleName(): String = "index" // Geralmente é "index"
-
-      override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-      override val isNewArchEnabled: Boolean = DefaultNewArchitectureEntryPoint.fabricEnabled
-      override val isHermesEnabled: Boolean = DefaultNewArchitectureEntryPoint.hermesEnabled
-    }
   )
 
   override val reactHost: ReactHost
-    get() = ReactNativeHostWrapper.createReactHost(
-      this.applicationContext,
-      reactNativeHost
-    )
+    get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, /* native exopackage */ false)
+    SoLoader.init(this, false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // Carrega bibliotecas da Nova Arquitetura
-      DefaultNewArchitectureEntryPoint.load()
+      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      load()
     }
-    ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
-    
-    // Adicionado pelo Expo
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
+  }
+
+  override fun onConfigurationChanged(newConfig: Configuration) {
+    super.onConfigurationChanged(newConfig)
+    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
   }
 }
